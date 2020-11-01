88fe976acc9bcc804aa97061b340ae1deed2ef1a
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2020-11-01 13:37:48 +0100

    Initial commit

---
+++ src/com/programacion/Main.java
@@ -0,0 +1,9 @@
+package com.programacion;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hola Programacion");
+    }
+}
