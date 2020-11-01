ab4e1b0cda847fdbf2487eb7ce8f7d8a69ab38ab
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2020-11-01 13:53:05 +0100

    Primer commit

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hola Programacion");
+        System.out.println("crear commit y hacer push");
     }
 }
